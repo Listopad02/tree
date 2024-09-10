eb82260107804060f32962c15fb6d62d27f8aeb0
Kirill <123ayetvbass@gmail.com>
2024-09-10 13:06:45 +0300

    fix: исправил отображение изменяемых свойств

--- src/components/TreeOptions/TreeOptions.tsx
+++ src/components/TreeOptions/TreeOptions.tsx
@@ -1,4 +1,4 @@
-import { Tabs } from "antd"
+import { Tabs, Select } from "antd"
 import type { TabsProps } from 'antd';
 import { useAppSelector } from "../../store/hooks";
 import { Properties } from "../../types/types";
@@ -13,13 +13,25 @@ const Text = styled.p`
     margin: 0;
     font-size: 16px;
 `
+const StyledSelect = styled(Select)`
+    width: 100%;
+    max-width: 300px;
+`
 
 export const TreeOptions = () => {
     const { itemProperties }: { itemProperties: Properties[] | [] } = useAppSelector(state => state.tree)
     const items: TabsProps['items'] = itemProperties.map(item => ({
         key: item.id,
         label: item.tabname,
-        children: <Container>{item.options.map(option => <Text>{option}</Text>)}</Container>
+        children: item.editable !== true ?
+            <Container>
+                {item.options.map(option => <Text>{option}</Text>)}
+            </Container> :
+            <Container>
+                <StyledSelect defaultValue={item.selected}>
+                    {item.options.map(option => <Select.Option value={option}>{option}</Select.Option>)}
+                </StyledSelect>
+            </Container>
     }))
 
     return <Tabs defaultActiveKey="0" items={items} />
